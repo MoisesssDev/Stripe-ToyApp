073145fe58f25ce485d12cfa54c193abf11f9b32
Moises Almeida <moisesssprg@gmail.com>
2024-11-22 16:11:00 -0300

    adiciona novas rotas para webhook

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   resources :stores
   resources :experiences
+  resources :webhooks, only: [ :create ]
   resource :stripe_account do
     get :dashboard, to: "stripe_accounts#dashboard", as: "dashboard"
     post :finish, to: "stripe_accounts#finish_stripe_account", as: "finish"
   end
   devise_for :users
   root to: "home#index"
+
+  namespace :webhooks do
+    resources :stripe, only: [ :create ]
+  end
 end
