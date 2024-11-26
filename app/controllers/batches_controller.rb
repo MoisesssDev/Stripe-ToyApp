60631eb18a99c94862bc4951b3fe1958d0be56a7
Moises Almeida <moisesssprg@gmail.com>
2024-11-26 14:23:10 -0300

    adiciona rota, controller e views para batch

---
+++ app/controllers/batches_controller.rb
@@ -0,0 +1,8 @@
+class BatchesController < ApplicationController
+  def new
+    @batch = Batch.new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :batches, only: [ :new, :create ]
   resources :stores
   resources :experiences
 
