cd0edb8fd34c5c082a7957f547ed006532db5987
Moises Almeida <moisesssprg@gmail.com>
2024-11-26 15:49:43 -0300

    finaliza o controller batches

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,8 +1,25 @@
 class BatchesController < ApplicationController
+  before_action :set_experience, only: [ :new ]
   def new
     @batch = Batch.new
   end
 
   def create
+    @batch = Batch.new(batch_params)
+    if @batch.save
+      redirect_to experience_path(@batch.experience), notice: "Batch was successfully created."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_experience
+    @experience = Experience.find(params[:experience_id])
+  end
+
+  def batch_params
+    params.require(:batch).permit(:price, :experience_id)
   end
 end
